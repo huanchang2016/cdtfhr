9dfc8494f918f0e3e63176eb1f2fea1584a871ab
zhanghanchang <zhanghanchang@cdtfhr.com>
2020-10-12 10:52:23 +0800

    新增判断搜索条件是否为json 格式

--- src/app/shared/pipe/keywords-search-higlight.pipe.ts
+++ src/app/shared/pipe/keywords-search-higlight.pipe.ts
@@ -10,24 +10,45 @@ export class KeywordsSearchHiglightPipe implements PipeTransform {
     private activatedRoute: ActivatedRoute
   ) {
     this.activatedRoute.queryParams.subscribe(params => {
-      if(params && params.keywords) {
-        this.keywords = JSON.parse(params.keywords);
+      if (params && params.keywords) {
+        // this.keywords = JSON.parse(params.keywords);
+        const isJson: boolean = this.isJSON(params.keywords);
+        if (isJson) {
+          this.keywords = JSON.parse(params.keywords);
+        }
       }
     });
   }
 
+  isJSON(str: any) { // 判断搜索条件是否为字符串
+    if (typeof str == 'string') {
+      try {
+        const obj = JSON.parse(str);
+        if (typeof obj == 'object' && obj) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (e) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
   transform(value: string): any {
-    let str:any = '';
-    if(this.keywords.length != 0) {
+    let str: any = '';
+    if (this.keywords.length != 0) {
       for (let i = 0; i < this.keywords.length; i++) {
         const element = this.keywords[i];
-        if(value.includes(element)) {
+        if (value.includes(element)) {
           const reg = eval('/' + element + '/g');
           value = value.replace(reg, `<span class="text-white bg-green">${element}</span>`);
         }
       }
       str = value;
-    }else {
+    } else {
       str = value;
     }
     return str;
